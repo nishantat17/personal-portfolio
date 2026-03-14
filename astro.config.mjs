b660c87189b4475e7427a49a57febc5c3f67960e
Nishant Bhagat <nishant17bhagat@outlook.com>
2026-03-14 15:07:21 +0530

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    fonts: [{
+        provider: fontProviders.google(),
+        name: "JetBrains Mono",
+        cssVariable: "--jetbrains-mono",
+        weights: [400],
+        styles: ["normal"]
+    },
+    {
+        provider: fontProviders.google(),
+        name: "Roboto",
+        cssVariable: "--roboto",
+        weights: [400],
+        styles: ["normal"],
+    },
+    {
+        provider: fontProviders.google(),
+        name: "Bitcount",
+        cssVariable: "--bitcount",
+        weights: [400, 500, 700],
+        styles: ["normal"],
+    }]
+});
